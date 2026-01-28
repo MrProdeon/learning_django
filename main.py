46edefd3a8d4318e165b52e5a683bd360747a2d2
Prodeon <prodeon21@gmail.com>
2026-01-28 15:08:02 +0300

    Убраны лишние комментарии, которые были добавлены для понимания кода

--- main.py
+++ main.py
@@ -14,19 +14,14 @@ class MyServer(BaseHTTPRequestHandler):
             self.wfile.write(bytes(file.read().encode("utf-8")))
 
     def do_POST(self):
-        # Считаем длину тела
         content_length = int(self.headers['Content-Length'])
-        # Читаем тело запроса
         post_data = self.rfile.read(content_length).decode('utf-8')
-        # Преобразуем в словарь
         data = parse_qs(post_data)
         name = data.get('name', [''])[0]
         email = data.get('email', [''])[0]
 
-        # Обработка данных
         print(f"Получены данные: Имя={name}, Email={email}")
 
-        # Ответ пользователю
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
